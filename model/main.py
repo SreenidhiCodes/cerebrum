44eab847f3a2899476a27fe982b57c7e4f6f5ee0
Sreenidhi RVNS <sreevanijagan@gmail.com>
2026-02-07 21:01:47 +0530

    Refactor memory management to include permanent memory

--- model/main.py
+++ model/main.py
@@ -1,18 +1,26 @@
 from model.memorymanager import TemporaryMemoryManager
-from model.memoryclassifier import should_create_temp_memory
+from model.permanentmemorymanager import PermanentMemoryManager
+from model.memoryclassifier import is_permanent_memory, is_task_related
 
 
 def process_input(user_text: str):
-    manager = TemporaryMemoryManager()
+    temp_manager = TemporaryMemoryManager()
+    perm_manager = PermanentMemoryManager()
 
-    if should_create_temp_memory(user_text, manager.has_active_memory()):
-        mem_id = manager.create_new_memory(user_text)
+    
+    if is_permanent_memory(user_text):
+        perm_manager.add_memory(user_text)
+        print("[System] Stored as PERMANENT memory")
+        return
+
+   
+    if is_task_related(user_text) and not temp_manager.has_active_memory():
+        mem_id = temp_manager.create_new_memory(user_text)
         print(f"[System] New temporary memory created: #{mem_id}")
 
-    elif manager.has_active_memory():
-        manager.append_to_active(user_text)
+    elif temp_manager.has_active_memory():
+        temp_manager.append_to_active(user_text)
         print("[System] Appended to active temporary memory")
 
     else:
         print("[System] No memory stored")
-
