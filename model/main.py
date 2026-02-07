46bfc0ce1e24ec71750f5a5d0887f71732cac63e
Sreenidhi RVNS <sreevanijagan@gmail.com>
2026-02-07 20:57:12 +0530

    Implement temporary memory management in input processing

--- model/main.py
+++ model/main.py
@@ -1,1 +1,18 @@
+from model.memorymanager import TemporaryMemoryManager
+from model.memoryclassifier import should_create_temp_memory
+
+
+def process_input(user_text: str):
+    manager = TemporaryMemoryManager()
+
+    if should_create_temp_memory(user_text, manager.has_active_memory()):
+        mem_id = manager.create_new_memory(user_text)
+        print(f"[System] New temporary memory created: #{mem_id}")
+
+    elif manager.has_active_memory():
+        manager.append_to_active(user_text)
+        print("[System] Appended to active temporary memory")
+
+    else:
+        print("[System] No memory stored")
 
