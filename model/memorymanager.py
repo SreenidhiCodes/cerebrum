4d68ed60c6e8693d22bd6d620c27d98c4c927b8c
Sreenidhi RVNS <sreevanijagan@gmail.com>
2026-02-11 14:24:47 +0530

    Refactor TemporaryMemoryManager methods
    Refactor memory management methods to improve clarity and handling of active memory checks.

--- model/memorymanager.py
+++ model/memorymanager.py
@@ -8,29 +8,45 @@ TEMP_EXPIRY_DAYS = 30
 
 class TemporaryMemoryManager:
     def __init__(self):
+        self._ensure_storage()
         self._load()
 
-    def _load(self):
+    def _ensure_storage(self):
+        if not os.path.exists("memorystore"):
+            os.makedirs("memorystore")
+
         if not os.path.exists(TEMP_MEMORY_PATH):
-            self.data = {
-                "counter": 0,
-                "active_memory_id": None,
-                "memories": {}
-            }
-            self._save()
-        else:
-            with open(TEMP_MEMORY_PATH, "r") as f:
-                self.data = json.load(f)
+            with open(TEMP_MEMORY_PATH, "w") as f:
+                json.dump({
+                    "counter": 0,
+                    "active_memory_id": None,
+                    "memories": {}
+                }, f, indent=4)
+
+    def _load(self):
+        with open(TEMP_MEMORY_PATH, "r") as f:
+            self.data = json.load(f)
 
     def _save(self):
         with open(TEMP_MEMORY_PATH, "w") as f:
             json.dump(self.data, f, indent=4)
 
     def has_active_memory(self):
-        return self.data["active_memory_id"] is not None
+        mem_id = self.data.get("active_memory_id")
+        if not mem_id:
+            return False
+
+        memory = self.data["memories"].get(mem_id)
+        if not memory:
+            return False
+
+        if datetime.utcnow() > datetime.fromisoformat(memory["expires_at"]):
+            self.end_active_memory()
+            return False
+
+        return True
 
     def create_new_memory(self, initial_text, origin_turn):
-        
         self.data["counter"] += 1
         mem_id = f"T{self.data['counter']}"
 
@@ -49,15 +65,19 @@ class TemporaryMemoryManager:
 
         self._save()
         return mem_id
-        
 
     def append_to_active(self, text, turn_id):
-        mem_id = self.data["active_memory_id"]
+        mem_id = self.data.get("active_memory_id")
         if not mem_id:
             return None
 
-        self.data["memories"][mem_id]["content"].append(text)
-        self.data["memories"][mem_id]["last_used_turn"] = turn_id
+        memory = self.data["memories"].get(mem_id)
+        if not memory:
+            return None
+
+        memory["content"].append(text)
+        memory["last_used_turn"] = turn_id
+
         self._save()
         return mem_id
 
@@ -66,8 +86,7 @@ class TemporaryMemoryManager:
         self._save()
 
     def get_active_memory(self):
-        mem_id = self.data["active_memory_id"]
+        mem_id = self.data.get("active_memory_id")
         if not mem_id:
             return None
         return self.data["memories"].get(mem_id)
-
